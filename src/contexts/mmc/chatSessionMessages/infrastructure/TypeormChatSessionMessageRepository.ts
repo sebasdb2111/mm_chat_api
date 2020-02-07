34e93910a4038706a7099d7fad4cb28a1c0091f1
Sebastian Diez Buades <sebastian.diez@mediamotive.es>
2020-02-07 12:05:20 +0100

    refactor repository with try catch

--- src/contexts/mmc/chatSessionMessages/infrastructure/TypeormChatSessionMessageRepository.ts
+++ src/contexts/mmc/chatSessionMessages/infrastructure/TypeormChatSessionMessageRepository.ts
@@ -2,42 +2,50 @@ import {getRepository}              from 'typeorm';
 import ChatSessionMessageRepository from '../domain/ChatSessionMessageRepository';
 import {ChatSessionMessage}         from '../domain/entity/ChatSessionMessage';
 import {EntitiesForRelationEnum}    from '../../shared/domain/EntitiesForRelationEnum';
-import {Customer} from '../../customers/domain/entity/Customer';
-import {ChatSession} from '../../chatSessions/domain/entity/ChatSession';
 
 export default class TypeormChatSessionMessageRepository implements ChatSessionMessageRepository
 {
     async findOneOrFail(id: number): Promise<ChatSessionMessage>
     {
-        const chatSessionMessageRepository           = getRepository(ChatSessionMessage);
-        const chatSessionMessage: ChatSessionMessage = await chatSessionMessageRepository.findOneOrFail(
-            id,
-            {
-                relations: [
-                    EntitiesForRelationEnum.CHATSESSION,
-                    EntitiesForRelationEnum.CUSTOMER,
-                    EntitiesForRelationEnum.PSYCHIC,
-                    EntitiesForRelationEnum.USER
-                ]
-            });
-        return Promise.resolve(chatSessionMessage);
+        try{
+            const chatSessionMessageRepository           = getRepository(ChatSessionMessage);
+            const chatSessionMessage: ChatSessionMessage = await chatSessionMessageRepository.findOneOrFail(
+                id,
+                {
+                    relations: [
+                        EntitiesForRelationEnum.CHATSESSION,
+                        EntitiesForRelationEnum.CUSTOMER,
+                        EntitiesForRelationEnum.PSYCHIC,
+                        EntitiesForRelationEnum.USER
+                    ]
+                });
+            return Promise.resolve(chatSessionMessage);
+        }
+        catch (error) {
+            return Promise.reject(error);
+        }
     }
 
     async findConversation(id: number): Promise<ChatSessionMessage[]>
     {
-		console.log(id);
-        const chatSessionMessageRepository = getRepository(ChatSessionMessage);
-        const chatSessionMessage: any      = await chatSessionMessageRepository
-            .createQueryBuilder('conversation')
-            .leftJoinAndSelect('conversation.chatSession', 'chatSession')
-            .leftJoinAndSelect('conversation.customer', 'customer')
-            .leftJoinAndSelect('conversation.psychic', 'psychic')
-            .leftJoinAndSelect('conversation.user', 'user')
-			.where('chatSessionId = :chatSession', {chatSession: id})
-            .orderBy('conversation.id', 'ASC')
-            .getMany();
+        try {
+			const chatSessionMessageRepository = getRepository(ChatSessionMessage);
+			const chatSessionMessage: any      = await chatSessionMessageRepository
+				.createQueryBuilder('conversation')
+				.leftJoinAndSelect('conversation.chatSession', 'chatSession')
+				.leftJoinAndSelect('conversation.customer', 'customer')
+				.leftJoinAndSelect('conversation.psychic', 'psychic')
+				.leftJoinAndSelect('conversation.user', 'user')
+				.where('chatSessionId = :chatSession', {chatSession: id})
+				.orderBy('conversation.id', 'ASC')
+				.getMany();
 
-        return Promise.resolve(chatSessionMessage);
+			return Promise.resolve(chatSessionMessage);
+        }
+        catch (error) {
+			return Promise.reject(error);
+		}
+
     }
 
     async save(chatSessionMessage: ChatSessionMessage): Promise<ChatSessionMessage>
@@ -54,7 +62,12 @@ export default class TypeormChatSessionMessageRepository implements ChatSessionM
 
     async updateDeleted(id: number, chatSessionMessage: ChatSessionMessage): Promise<void>
     {
-        const chatSessionMessageRepository = getRepository(ChatSessionMessage);
-        await chatSessionMessageRepository.update(id, {deleted: chatSessionMessage.deleted});
+        try {
+			const chatSessionMessageRepository = getRepository(ChatSessionMessage);
+			await chatSessionMessageRepository.update(id, {deleted: chatSessionMessage.deleted});
+		}
+		catch (error) {
+            return Promise.reject(error)
+		}
     }
 }
