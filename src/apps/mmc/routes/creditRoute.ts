2a71e14b2d908c816e63defaa9674eeaee039ab6
Sebastian Diez Buades <sebastian.diez@mediamotive.es>
2020-02-13 13:51:21 +0100

    servicio para conseguir los coins disponible por chat y socket para el estado de conexion tanto individual como de la lista en vivo

--- src/apps/mmc/routes/creditRoute.ts
+++ src/apps/mmc/routes/creditRoute.ts
@@ -1,18 +1,16 @@
-// import {Router}                 from 'express';
-// import container                from '../config/dependency-injection';
-// import {CheckAuthentication}    from '../../../contexts/shared/application/CheckAuthentication';
-// import {CreditCreateController} from '../controllers/credit/CreditCreateController';
-// import {CheckRole}              from '../../../contexts/shared/application/CheckRole';
-//
-// const router                                         = Router();
-// const creditCreateController: CreditCreateController = container.get('Apps.mmc.controllers.credit.CreditCreateController');
-//
-// router
-//     .post(
-//         '/',
-//         CheckAuthentication,
-//         CheckRole['ADMIN'],
-//         creditCreateController.run.bind(creditCreateController)
-//     );
-//
-// export default router;
+import {Router}                 from 'express';
+import container                from '../config/dependency-injection';
+import {CheckAuthentication}    from '../../../contexts/shared/application/CheckAuthentication';
+import {CreditSumGetController} from '../controllers/credit/CreditSumGetController';
+
+const router                                         = Router();
+const creditCreateController: CreditSumGetController = container.get('Apps.mmc.controllers.credit.CreditSumGetController');
+
+router
+    .get(
+        '/sum-by-customer-and-psychic',
+        CheckAuthentication,
+        creditCreateController.run.bind(creditCreateController)
+    );
+
+export default router;
